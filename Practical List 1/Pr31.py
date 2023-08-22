0e3a32e1f4355b510f96719b31b957dbfba337de
Kushal129 <21bmiit129@outlook.com>
2023-08-22 18:51:46 +0530

    init

--- Practical List 1/Pr31.py
+++ Practical List 1/Pr31.py
@@ -1,1 +1,25 @@
-# baki 
+# To find total number of odd digit, even digit, sum of odd digit and sum of even 
+# digit from the given number.
+
+# ( input number ne chuta padvana ODD & EVEN ma and then eki no sum aalgthi ne beki no sum aalgthi)
+
+n = int(input("Enter Number : "));
+odd = 0
+even = 0
+x = 0
+odd_count = 0
+even_count = 0
+
+while n > 0: #n greater 0 loop runig 
+    x = n % 10 #n remainder (1234 to r= 4 so that x = 4)
+    if x % 2 == 0:
+        even = even + x
+        even_count +=1 
+    else:
+        odd += x
+        odd_count += 1
+    n = n//10 # 1234 chaltu rese ne x ma 4 jrese so that intiger divition kari // thi , so 123 quotient are remaing...
+    
+print ('Even sum :- ', even  ,'|','Odd sum:- ',odd )
+print ('Total Even number :- ',even_count  ,'|','Total Odd number :- ' , odd_count  )
+
--- Practical List 1/Pr32.py
+++ Practical List 1/Pr32.py
@@ -1,1 +1,284 @@
-# baki 
+# Python  Patterns
+
+# P - 1 
+
+# n = int (input("Enter Number :- "))
+# for i in range(n): # 0 to n ROW |
+#     for j in range(n-i-1): # Column -
+#         print(" ",end="") # end bez ek line ma j jose _ _* space mate
+#     for j in range(i+1): # How many star whant row is 0 i what 1 star....
+#         print("*",end=" ") 
+#     print ()
+    
+# out put :-  
+"""
+    *
+   * *
+  * * *
+ * * * *
+* * * * * 
+
+""" 
+# ------------------------------------------------------------------------
+
+# P - 2
+
+# n = int (input("Enter Number :- "))
+#     # 0 to n ROW |
+# for i in range(n): 
+#         # Column -
+#     for j in range(i):
+#         print(" ",end="") # end bez ek line ma j jose _ _* space mate
+#     for j in range(n-i): # How many star whant row is 0 i what 5 star , 1 so 4
+#         print("*",end=" ") 
+#     print()
+    
+# out put :-
+"""
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+"""
+# ------------------------------------------------------------------------
+
+# P - 3 
+str1 = input("Enter String: ")
+length = len(str1)
+
+# Reverse 
+for i in range(length - 1, -1, -1):
+    if i % 2 == 0:
+        for j in range(i + 1):
+            print(str1[j], end=" ")
+    else:
+        for j in range(i, -1, -1):
+            print(str1[j], end=" ")
+    print()
+
+
+# out put :- 
+
+"""
+12345
+1234
+123
+12
+1
+"""
+
+# ------------------------------------------------------------------------
+
+# P - 4
+
+# n = int(input("Enter Number: "))
+
+# # Upper 
+# for i in range(n , 0 , -1): # row Dicri..form ma n to.. n =554321
+#     for j in range(i,0,-1): # Left-Top 
+#         print("*", end=" ")
+#     for j in range( 2 * (n - i)): # middle ma space kare 
+#         print(" ", end=" ")
+#     for j in range(i,0,-1): # Right Top
+#         print("*", end=" ")
+#     print()
+
+# # Lower 
+# for i in range(1,n): # row  asenig...
+#     for j in range(i + 1): # left down 
+#         print("*", end=" ")
+#     for j in range(2 * (n-i-1) ): # middle ma space 
+#         print(" ", end=" ")
+#     for j in range(i + 1): # right down 
+#         print("*", end=" ")
+#     print()
+
+# out put :-
+
+"""
+
+* * * * * * * * * * 
+* * * *     * * * * 
+* * *         * * * 
+* *             * * 
+*                 * 
+* *             * * 
+* * *         * * * 
+* * * *     * * * * 
+* * * * * * * * * * 
+    
+"""
+
+
+# ----------------------------------------------------------------
+
+
+# P - 5 
+
+# n = int (input("Enter Number: "))
+# for i in range(n - 1 ):
+#     for j in range(i , n ):
+#         print(" " , end="")
+#     for j in range(i):
+#         print("*", end="")
+#     for j in range(i+1):
+#         print("*" , end="")
+#     print()
+    
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" " , end="")
+#     for j in range(i,n-1):
+#         print("*", end="")
+#     for j in range(i,n):
+#         print("*" , end="")
+#     print()
+    
+# out put :- 
+
+"""  
+     *
+    ***
+   *****
+  *******
+ *********
+  *******
+   *****
+    ***
+     *
+"""
+
+# ------------------------------------------------------------------------
+
+# P - 6 
+
+# n = int (input("Enter Number: "))
+# count = 1 
+# for i in range(n , 0 , -1):
+#     for j in range(1,i+1):
+#         print(count%2 , end=" ")
+#     print()
+#     if (i%2 == 0):
+#         count=1
+#     else:
+#         count=0
+        
+# out put :-
+"""
+
+1 1 1 1 1 
+0 0 0 0 
+1 1 1 
+0 0 
+1 
+
+"""
+
+#----------------------------------------------------------------
+
+# P - 7 
+         
+        #  BAKII
+
+#----------------------------------------------------------------
+
+# P - 8 
+
+# n = int (input("Enter Number: "))
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(n-j, end="")
+#     print()
+    
+
+# out put :-
+"""
+
+5
+54
+543
+5432
+54321
+
+"""
+
+#----------------------------------------------------------------
+
+# P - 9
+
+# n = int (input("Enter Number: "))
+# for i in range(1,n+1):
+#     for j in range(1 , i+1):
+#         print(j%2, end=" ")
+#     print()
+
+# out put :- 
+
+"""
+
+1 
+1 0 
+1 0 1 
+1 0 1 0 
+1 0 1 0 1 
+    
+"""
+
+#----------------------------------------------------------------
+
+# P - 10
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, n + 1):
+#     for j in range(n, n - i, -1):
+#         print(j, end="")
+#     for j in range(j,0,-1): 
+#         print(n, end="")
+#     print()
+
+# out put :- 
+
+"""
+    BAkIIII
+    
+"""
+
+
+#----------------------------------------------------------------
+
+# P - 11
+
+# str1 =input("Enter String: ")
+# # we can find length of the input string
+# length = len (str1)
+# for i in range(length): # row
+#     for j in range(i+1): # column
+#         print(str1[j], end=" ")
+#     print()
+
+# out put :- 
+
+"""
+
+A 
+A B
+A B C
+A B C D
+A B C D E
+
+"""
+
+#----------------------------------------------------------------
+
+# P - 12
+
+# str1 =input("Enter String: ")
+# length = len(str1)
+# for i in range(-1,length,): # row
+#     for j in range(length - i): # column
+#         print(str1[i], end=" ")
+#     print()
+
+
